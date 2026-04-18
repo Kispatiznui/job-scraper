3303e8016c5865a09f92ef5ccb8f72230aa02408
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-17 19:41:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+import logging
+import json
+from scraper.fetcher import fetch_jobs
+from scraper.parser import parse_jobs
+from scraper.storage import save_jobs
+from scraper.filters import filter_jobs
+
+URL = "https://realpython.github.io/fake-jobs”
+
+def setup_logging():
+    logging.basicConfig(
+        filename="logs/app.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+def load_config():
+    with open("config.json") as f:
+        return json.load(f)
+
+def main():
+    setup_logging()
+    config = load_config()
+
+    logging.info("Starting job scraping process")
+
+    html = fetch_jobs(URL)
+    jobs = parse_jobs(html)
+
+    logging.info(f"Jobs found: {len(jobs)}")
+
+    filtered_jobs = filter_jobs(jobs, config)
+
+    logging.info(f"Jobs after filtering: {len(filtered_jobs)}")
+
+    save_jobs(filtered_jobs)
+
+    print("✅ Jobs saved successfully")
+
+if __name__ == "__main__":
+    main()
 
